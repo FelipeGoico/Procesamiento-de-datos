e8813164150fb5c64623fe22afb41bd1b21e9ffe
FelipeGoico <efeg.py@gmail.com>
2025-09-09 21:43:52 -0300

    agregue informacion a resumen

---
+++ app/pages/6_Resumen.py
@@ -0,0 +1,10 @@
+import app import st
+
+
+st.write("""# Resumen del Examen Final
+Este examen final abarca diversas técnicas de procesamiento de datos y análisis estadístico, incluyendo Análisis
+Exploratorio de Datos (EDA), Análisis Discriminante Lineal (LDA) y Análisis de Componentes Principales (PCA). A continuación,
+se presenta un resumen de cada una de estas técnicas y sus aplicaciones.
+""")
+st.set_page_config(page_title="Resumen",
+                   page_icon=":bar_chart:", layout="wide")
